a2fb114395a2d2445d8fbcbf97bd8f377d8d5922
toshi0383 <t.suzuki326@gmail.com>
2019-11-14 21:08:37 +0900

    support swift 5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -21,6 +21,5 @@ let package = Package(
         .testTarget(
             name: "ConcurrencyTests",
             dependencies: ["Concurrency"]),
-    ],
-    swiftLanguageVersions: [4]
+    ]
 )
